fb74acbc353c67dcb4e5116705210ebdeb4b7e33
AndreRodrigues884 <andreprodrigues0512@gmail.com>
2025-08-25 12:35:48 +0100

    Restore

--- backend/src/middlewares/isAdmin.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  if (req.user?.type !== 'admin') {
-    return res.error('NOT_AUTHORIZED_ADMIN');
-  }
-  next();
-};
--- backend/src/routes/achievement.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const achievementController = require('../controllers/achievement.controllers');
-const auth = require('../middlewares/auth');
-
-router.get('/', auth, achievementController.getAchievements);
-router.get('/:type', auth, achievementController.getAchievementByType);
-
-module.exports = router;
